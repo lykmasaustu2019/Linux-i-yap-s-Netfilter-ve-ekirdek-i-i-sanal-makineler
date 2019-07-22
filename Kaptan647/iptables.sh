36bb55497eda1b75b5d967b57bd8db868b5d8399
lykmasaustu2019 <lykmasaustu2019@protonmail.com>
2019-07-22 21:38:04 +0300

    Timuçin - Iptables dosyası düzenlendi.

--- Kaptan647/iptables.sh
+++ Kaptan647/iptables.sh
@@ -1,16 +1,33 @@
-# PREROUTING zincirinin raw tablosunda 80 portuna gelen tcp isteklerini düşür
-iptables -t raw -I PREROUTING -p tcp --dport 80 -j DROP
+# PREROUTING zincirinin raw tablosunda 80 portuna gelen tcp paketlerini düşür
 
-iptables -I INPUT -p tcp --dport 80 -j DROP
-iptables -I OUTPUT -p tcp --sport 80 -j DROP
-iptables -t nat -I PREROUTING -p tcp --dport 80 -j DNAT --to-destination 1.1.1.1
+iptables -t raw -A PREROUTING -p tcp --dport 80 -j DROP
+
+#INPUT zincirinin filter(default) tablosunda 80 portuna gelen tcp paketlerini düşür
+
+iptables -A INPUT -p tcp --dport 80 -j DROP
+
+#OUTPUT zincirinin filter(default) tablosunda 80 portundan giden tcp paketlerini düşür
+
+iptables -A OUTPUT -p tcp --sport 80 -j DROP
+
+#PREROUTING zincirinin nat tablosunda 80 portuna gelen tcp paketlerini 1.1.1.1 iipsine yönlendir
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 1.1.1.1
+
+#INPUT zincirinde 80 portuna gelen tcp paketlerini BLOCKER zincirine gönder ve düşür
 
 iptables -N BLOCKER
-iptables -P BLOCKER DROP
 iptables -A INPUT -p tcp --dport 80 -j BLOCKER
-iptables -N BLOCKERv2
-iptables -P BLOCKERV2 DROP
-iptables -A BLOCKER -p tcp --dport 80 -j BLOCKERv2
+iptables -A BLOCKER -j DROP
 
-iptables -I INPUT -p tcp --dport 80 -j REJECT
+#Custom zincire zincir bağla
+
+iptables -N BLOCKER
+iptables -A INPUT -p tcp --dport 80 -j BLOCKER
+iptables -A BLOCKER -j BLOCKERV2
+iptables -N BLOCKERV2
+iptables -A BLOCKERV2 -j DROP
+
+#INPUT zincirinde 80 portuna gelen tcp paketlerini rejectle
+
+iptables -A INPUT -p tcp --dport 80 -j REJECT
 
