c625c7a714a616680b168bc037c18bff4462f777
lykmasaustu2019 <53182141+lykmasaustu2019@users.noreply.github.com>
2019-07-25 16:03:33 +0300

    Update server.sh

--- fuat/configs/server.sh
+++ fuat/configs/server.sh
@@ -1,11 +1,9 @@
 #-----interface isimleri-----
 ip link set enp0s3 down
 ip link set enp0s3 name sr0
-
 #-----interface ipleri------
 ip addr add 10.3.0.2/24 dev sr0
 ip link set sr0 up
-
 #------routing------
-
+ip route add default via 10.3.0.1 dev sr0
 #------iptabables------
