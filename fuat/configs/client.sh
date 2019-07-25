3960778d6b90fc987ded9d9e6d352636f4f7a9a4
lykmasaustu2019 <53182141+lykmasaustu2019@users.noreply.github.com>
2019-07-25 16:03:18 +0300

    Update client.sh

--- fuat/configs/client.sh
+++ fuat/configs/client.sh
@@ -1,11 +1,9 @@
 #-----interface isimleri-----
 ip link set enp0s3 down
 ip link set enp0s3 name cg0
-
 #-----interface ipleri------
 ip addr add 10.1.0.2/24 dev cg0
 ip link set cg0 up
-
 #------routing-----
 ip route add default via 10.1.0.1 dev cg0
 #------iptabables-----
