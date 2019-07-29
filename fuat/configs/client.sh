087a1522282c220dd58b407962e34e083c1ca41b
lykmasaustu2019 <53182141+lykmasaustu2019@users.noreply.github.com>
2019-07-29 12:08:23 +0300

    Update client.sh

--- fuat/configs/client.sh
+++ fuat/configs/client.sh
@@ -7,3 +7,8 @@ ip link set cg0 up
 #------routing-----
 ip route add default via 10.1.0.1 dev cg0
 #------iptabables-----
+/sbin/iptables -P INPUT DROP
+/sbin/iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT    #related
+#-----ek ayarlar-------
+#/etc/hosts   
+#10.3.0.2	server
